905b524634ae580880a4d5bb67468d07a68688a6
r1c0n <reconispog@gmail.com>
2023-09-23 11:49:45 -0700

    rewrite

--- source/main.c
+++ source/main.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
-#include <ogcsys.h>
 #include <gccore.h>
 #include <wiiuse/wpad.h>
 #pragma endregion
@@ -12,15 +10,19 @@
 static u32 *xfb;
 static GXRModeObj *rmode;
 
-void Initialize()
+void InitializeVideo()
 {
 	VIDEO_Init(); // Initialize video
-
 	rmode = VIDEO_GetPreferredMode(NULL);
 
 	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-	console_init(xfb, 20, 20, rmode->fbWidth, rmode->xfbHeight, rmode->fbWidth * VI_DISPLAY_PIX_SZ);
+	if (!xfb)
+	{
+		printf("Error allocating framebuffer\n");
+		exit(EXIT_FAILURE);
+	}
 
+	console_init(xfb, 20, 20, rmode->fbWidth, rmode->xfbHeight, rmode->fbWidth * VI_DISPLAY_PIX_SZ);
 	VIDEO_Configure(rmode);
 	VIDEO_SetNextFramebuffer(xfb);
 	VIDEO_SetBlack(FALSE);
@@ -30,11 +32,18 @@ void Initialize()
 		VIDEO_WaitVSync();
 }
 
+void PowerOffWii()
+{
+	SYS_ResetSystem(SYS_POWEROFF_STANDBY, 0, 0); // Power Wii off
+}
+
 #pragma endregion
 
+#pragma region Main
 int main()
 {
-	Initialize();								 // Run initialize function
-	SYS_ResetSystem(SYS_POWEROFF_STANDBY, 0, 0); // Power Wii off
-	return 0;									 // Exit with code 0
+	InitializeVideo(); // Run initialize function
+	PowerOffWii();	   // Power Wii off
+	return 0;		   // Exit with code 0
 }
+#pragma endregion
